97cf1d9ecf63cca80c8e7a023d4b2d499efb784c
mkurapov <maxkurapov@gmail.com>
2016-07-06 01:18:33 -0600

    creation of audio object

--- src/index.js
+++ src/index.js
@@ -7,6 +7,7 @@ export default class SPWidget {
     this.artist = '';
     this.name = '';
     this.previewUrl = '';
+    this.player = new Audio();
 
     if (params)
     {
@@ -15,6 +16,38 @@ export default class SPWidget {
     else
     {
       console.error('No parameters were provided.');
+    }
+  }
+
+  togglePlay()
+  {
+    if (this.player.paused)
+    {
+      this.player.play();
+    }
+    else
+    {
+      this.player.pause();
+    }
+  }
+
+  buttonPlaying()
+  {
+    var playButton = document.getElementById('play-button');
+
+    if (playButton)
+    {
+      playButton.setAttribute("class", "playing");
+    }
+  }
+
+  buttonPaused()
+  {
+    var playButton = document.getElementById('play-button');
+
+    if (playButton)
+    {
+      playButton.setAttribute("class", "paused");
     }
   }
 
@@ -28,26 +61,38 @@ export default class SPWidget {
       this.artist = result.artists[0].name;
       this.imageUrl = result.album.images[0].url;
       this.previewUrl = result.preview_url;
+      this.player.src = result.preview_url;
+
+      this.player.onplay = () => this.buttonPlaying();
+      this.player.onpause = () => this.buttonPaused();
+
       console.log(this);
-
-    }).then(() => {
-
-      var widget = document.getElementById('SPWidget');
-      if (widget)
-      {
-          var htmlString = `<div class="name">${this.name}</div>`;
-          htmlString += `<img src=${this.imageUrl}></img>`;
-        htmlString += `<audio src=${this.previewUrl} controls></audio>`;
-          widget.innerHTML = htmlString;
-      }
-      else
-      {
-        console.error('No div with id "spotifyWidget" found');
-      }
-
-
+      this.displayPlayer();
 
     });
+  }
+
+  displayPlayer()
+  {
+    var widget = document.getElementById('SPWidget');
+    if (widget)
+    {
+      var htmlString = `<img src=${this.imageUrl}></img>`;
+      htmlString += `<div class="info">`;
+      htmlString += `<div class="artist">${this.artist}</div>`;
+      htmlString += `<div class="song-name">${this.name}</div>`;
+      htmlString += `<div id="play-button" class="paused"></div>`;
+      htmlString += `</div>`;
+
+      widget.innerHTML = htmlString;
+
+      var playButton = document.getElementById('play-button');
+      playButton.onclick = () => this.togglePlay();
+    }
+    else
+    {
+      console.error('No div with id "spotifyWidget" found');
+    }
   }
 
 }
